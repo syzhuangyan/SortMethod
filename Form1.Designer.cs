1c073a1351ccb0810804f75d93ae87544e6fdf5a
ZhuangYan <276470798@qq.com>
2016-09-28 11:40:31 +0800

    上传代码，实现基本的排序数据初始化功能
    实现了排序数据的个数设定，数据大小的随机生成，动态生成数据对应格式的按钮，控件内容的实现功能。

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SortMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BubbleCountList = new System.Windows.Forms.ComboBox();
+            this.CreateRandomBtn = new System.Windows.Forms.Button();
+            this.SortMethodList = new System.Windows.Forms.ComboBox();
+            this.StartSort = new System.Windows.Forms.Button();
+            this.SortStyleList = new System.Windows.Forms.ComboBox();
+            this.BubbleNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BubbleCountList
+            // 
+            this.BubbleCountList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BubbleCountList.FormattingEnabled = true;
+            this.BubbleCountList.Location = new System.Drawing.Point(66, 21);
+            this.BubbleCountList.Name = "BubbleCountList";
+            this.BubbleCountList.Size = new System.Drawing.Size(68, 20);
+            this.BubbleCountList.TabIndex = 0;
+            this.BubbleCountList.SelectedIndexChanged += new System.EventHandler(this.BubbleCountList_SelectedIndexChanged);
+            // 
+            // CreateRandomBtn
+            // 
+            this.CreateRandomBtn.Location = new System.Drawing.Point(161, 20);
+            this.CreateRandomBtn.Name = "CreateRandomBtn";
+            this.CreateRandomBtn.Size = new System.Drawing.Size(108, 23);
+            this.CreateRandomBtn.TabIndex = 1;
+            this.CreateRandomBtn.Text = "生产随机数";
+            this.CreateRandomBtn.UseVisualStyleBackColor = true;
+            this.CreateRandomBtn.Click += new System.EventHandler(this.CreateRandomBtn_Click);
+            // 
+            // SortMethodList
+            // 
+            this.SortMethodList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SortMethodList.FormattingEnabled = true;
+            this.SortMethodList.Location = new System.Drawing.Point(293, 21);
+            this.SortMethodList.Name = "SortMethodList";
+            this.SortMethodList.Size = new System.Drawing.Size(158, 20);
+            this.SortMethodList.TabIndex = 2;
+            // 
+            // StartSort
+            // 
+            this.StartSort.Location = new System.Drawing.Point(634, 21);
+            this.StartSort.Name = "StartSort";
+            this.StartSort.Size = new System.Drawing.Size(97, 23);
+            this.StartSort.TabIndex = 3;
+            this.StartSort.Text = "开始排序";
+            this.StartSort.UseVisualStyleBackColor = true;
+            // 
+            // SortStyleList
+            // 
+            this.SortStyleList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SortStyleList.FormattingEnabled = true;
+            this.SortStyleList.Location = new System.Drawing.Point(485, 22);
+            this.SortStyleList.Name = "SortStyleList";
+            this.SortStyleList.Size = new System.Drawing.Size(121, 20);
+            this.SortStyleList.TabIndex = 4;
+            // 
+            // BubbleNum
+            // 
+            this.BubbleNum.AutoSize = true;
+            this.BubbleNum.Location = new System.Drawing.Point(19, 24);
+            this.BubbleNum.Name = "BubbleNum";
+            this.BubbleNum.Size = new System.Drawing.Size(29, 12);
+            this.BubbleNum.TabIndex = 5;
+            this.BubbleNum.Text = "个数";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 480);
+            this.Controls.Add(this.BubbleNum);
+            this.Controls.Add(this.SortStyleList);
+            this.Controls.Add(this.StartSort);
+            this.Controls.Add(this.SortMethodList);
+            this.Controls.Add(this.CreateRandomBtn);
+            this.Controls.Add(this.BubbleCountList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SortMethod";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox BubbleCountList;
+        private System.Windows.Forms.Button CreateRandomBtn;
+        private System.Windows.Forms.ComboBox SortMethodList;
+        private System.Windows.Forms.Button StartSort;
+        private System.Windows.Forms.ComboBox SortStyleList;
+        private System.Windows.Forms.Label BubbleNum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SortMethod
+{
+    public partial class Form1 : Form
+    {
+        int Max = 20;
+        List<int> BubbleValue = new List<int>();
+
+        //冒泡排序(bubble sort)
+        //插入排序(insertion sort)
+        //归并排序(merge sort)
+        //桶排序(bucket sort)
+        //基数排序(Radix sort)
+        //二叉树排序(Binary tree sort)
+        //选择排序(selection sort)
+        //希尔排序(shell sort)
+        //堆排序(heapsort)
+        //快速排序(quicksort)
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < Max+1; i++)
+            {
+                BubbleCountList.Items.Add(i);
+            }
+            BubbleCountList.SelectedIndex = 10;
+
+
+            SortMethodList.Items.Add("冒泡排序");
+            SortMethodList.Items.Add("插入排序");
+            SortMethodList.Items.Add("归并排序");
+            SortMethodList.Items.Add("桶排序");
+            SortMethodList.Items.Add("基数排序");
+            SortMethodList.Items.Add("二叉树排序");
+            SortMethodList.Items.Add("选择排序");
+            SortMethodList.Items.Add("希尔排序");
+            SortMethodList.Items.Add("堆排序");
+            SortMethodList.Items.Add("快速排序");
+
+            SortStyleList.Items.Add("顺序执行");
+            SortStyleList.Items.Add("单步执行");
+        }
+
+        private void BubbleCountList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Max+1; i++)
+            {
+                Button BubbleBtn = this.Controls["Bubble" + i.ToString()] as Button;
+                if (BubbleBtn != null)
+                    this.Controls.Remove(BubbleBtn);
+            }
+
+            for (int i = 0; i < BubbleCountList.SelectedIndex+1; i++)
+            {
+                Button BubbleBtn = new Button();
+                BubbleBtn.Name = "Bubble" + i.ToString();
+                BubbleBtn.Text = "Bubble" + i.ToString();
+                BubbleBtn.Size = new Size(50,50);
+                BubbleBtn.Location = new Point(20 + i * 50, 70);
+                this.Controls.Add(BubbleBtn);
+            }
+        }
+
+        private void CreateRandomBtn_Click(object sender, EventArgs e)
+        {
+            BubbleValue.Clear();
+            for (int i = 0; i < BubbleCountList.SelectedIndex + 1; i++)
+            {
+                Button BubbleBtn = this.Controls["Bubble" + i.ToString()] as Button;
+                BubbleBtn.Text = GetRandomNum();
+                BubbleValue.Add(Convert.ToInt32(BubbleBtn.Text));
+            }
+        }
+
+        private string GetRandomNum()
+        {
+            string SrcString = Guid.NewGuid().ToString("N");
+            string DigitString = "";
+            for (int i = 0; i < SrcString.Length - 1; i++)
+            {
+                if (char.IsNumber(SrcString, i))
+                {
+                    DigitString += SrcString[i];
+                }
+                if (DigitString.Length == 3)
+                {
+                    break;
+                }
+            }
+            return DigitString;
+        }
+    }
+}
